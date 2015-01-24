fbdea72f5b07d477c3aaab49fb4d11c213a3a379
onda47 <paul.mesnilgrente@gmail.com>
2015-01-24 10:51:43 +0100

    ajout des fichiers sources

---
+++ sources_java/src/application/Main.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String message = "Hello World !";
+		System.out.println(message);
+
+	}
+
+}
